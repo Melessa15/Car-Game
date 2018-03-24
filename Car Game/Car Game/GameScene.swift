13e4b99e9e93400d63eff3db0a7813b3464a8b11
Cecil Costa <eduardo@conglomo.es>
2018-03-24 18:40:30 +0000

    Score and Moving Car, collision

--- Car Game/Car Game/GameScene.swift
+++ Car Game/Car Game/GameScene.swift
@@ -20,51 +20,53 @@ class GameScene: SKScene {
     let height = CGFloat(1334)
     let width = CGFloat(750)
     let roadSpeed = 5.0
+    var timer: Timer?
+    var score = 0
+    let scoreDisplay = SKLabelNode(text: "0")
+    
     
     override func didMove(to view: SKView) {
         
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -4)
+        
         self.setup1stRoad()
         self.setup2ndRoad()
+        self.addChild(self.scoreDisplay)
+        self.scoreDisplay.position = CGPoint(x: self.width / 2 - 20, y: self.height / 2 - 50)
         
-        playersCar.size = CGSize(width: playersCar.size.width / 4,
-                                 height: playersCar.size.height / 4)
+        playersCar.xScale = 0.25
+        playersCar.yScale = 0.25
         self.addChild(playersCar)
+        self.physicsWorld.contactDelegate = self
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let physicsBody = SKPhysicsBody(rectangleOf: self.playersCar.size)
+        physicsBody.contactTestBitMask = 0x00000001
+        //physicsBody.pinned = true
+        physicsBody.allowsRotation = false
+        physicsBody.affectedByGravity = false
+        self.playersCar.physicsBody = physicsBody
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: { [weak self] (_) in
+            self?.newCar()
+        })
     }
     
     func setup1stRoad() {
         self.addChild(road)
         self.road.size = CGSize(width: self.width, height: self.height)
+        self.road.zPosition = CGFloat(-1)
         let action1 = SKAction.moveBy(x: 0, y: -self.height, duration: self.roadSpeed)
         let action2 = SKAction.moveBy(x: 0, y: 2*self.height, duration: 0)
         let action3 = SKAction.moveBy(x: 0, y: -self.height, duration: self.roadSpeed)
         let sequence = SKAction.sequence([action1, action2, action3])
         let repetition = SKAction.repeatForever(sequence)
+        
         self.road.run(repetition)
     }
     
     func setup2ndRoad() {
         self.addChild(road2)
-        
+        self.road2.zPosition = CGFloat(-1)
         self.road2.size = CGSize(width: self.width, height: self.height)
         let action1 = SKAction.moveBy(x: 0, y: self.height, duration: 0)
         let action2 = SKAction.moveBy(x: 0, y: -2*self.height, duration: self.roadSpeed * 2)
@@ -74,29 +76,45 @@ class GameScene: SKScene {
         self.road2.run(repetition)
     }
     
+    func newCar() {
+        self.score += 5
+        let carType = arc4random() % 6 + 1
+        let node = SKSpriteNode(imageNamed: "car\(carType)")
+        node.name = "car"
+        let positionOffset = Int(arc4random()) % Int(self.width) - Int(self.width) / 2
+        
+        node.position = CGPoint(x: CGFloat(positionOffset), y: self.height / 2)
+        node.size = CGSize(width: 100, height: 200)
+        let action = SKAction.moveBy(x: 0, y: -(self.height + 100), duration: 3)
+        let actionRemove = SKAction.removeFromParent()
+        let sequence = SKAction.sequence([action, actionRemove])
+        node.run(sequence)
+        self.addChild(node)
+        
+        let physicsBody = SKPhysicsBody(rectangleOf: node.size)
+        physicsBody.contactTestBitMask = 0x00000002
+        //physicsBody.pinned = true
+        physicsBody.allowsRotation = true
+        
+        node.physicsBody = physicsBody
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: { [weak self] (_) in
+            self?.newCar()
+        })
+        
+    }
+    
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        self.playersCar.position = pos
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -121,6 +139,26 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        self.playersCar.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        self.enumerateChildNodes(withName: "car") { (node, stop) in
+            if node.intersects(self.playersCar) {
+                self.score -= 1
+                
+            }
+        }
+        self.scoreDisplay.text = "\(self.score)"
     }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        
+    }
+    
+    func didEnd(_ contact: SKPhysicsContact) {
+        print(contact.contactPoint)
+    }
+    
+    
 }
